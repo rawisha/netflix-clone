02567aede3864412e017ca377317c37a3085885f
Rawisha <rawvsmaw22@gmail.com>
2022-03-03 12:08:51 +0100

    push

---
+++ src/requests.js
@@ -0,0 +1,14 @@
+const API_KEY = "a029f1ddbddfacb99c10a7a65957a123";
+
+const requests = {
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en_us`,
+  fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=en_us`,
+  fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
+  fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
+  fetchHorrorMovies: `/discover/movie?api_key=${API_KEY}&with_genres=27`,
+  fetchRomanceMovies: `/discover/movie?api_key=${API_KEY}&with_genres=10749`,
+  fetchDocumentaries: `/discover/movie?api_key=${API_KEY}&with_genres=99`,
+};
+
+export default requests;
